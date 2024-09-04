573bd4d4e429c035ae29542b3123c36fcb4d8ff4
ShirishaKb <shirishakb@gmail.com>
2024-09-04 15:05:23 -0400

    sweetalertsanddatavalidated

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,11 @@
 // Read contract data from local storage
-function readContractsData(){
+function readContractsData() {
     const allContractsData = JSON.parse(localStorage.getItem('allContractsData') || '[]');
     return allContractsData
 }
 
 // Store the contract information to local storage as an array of objects
-function storeContractInfo(contractInfo){
+function storeContractInfo(contractInfo) {
     const allContractsData = readContractsData();
     allContractsData.push(contractInfo);
     localStorage.setItem('allContractsData', JSON.stringify(allContractsData));
@@ -14,54 +14,73 @@ function storeContractInfo(contractInfo){
 // Create redirect function
 function redirectPage(redirectUrl) {
     location.assign(redirectUrl);
-} 
+}
 
 // Function to remove a row from the local storage with confirmation
 function deleteRow(i) {
-    const userConfirmed = confirm("Are you sure you want to delete this contract?");
-    if (userConfirmed) {
-        const allContractsData = readContractsData();
-        allContractsData.splice(i, 1);
-        if (allContractsData.length === 0) {
-            noContracts();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "Do you want to delete this contract?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const allContractsData = readContractsData();
+            allContractsData.splice(i, 1);
+            if (allContractsData.length === 0) {
+                noContracts();
+            }
+            localStorage.setItem('allContractsData', JSON.stringify(allContractsData));
+            renderContractList();
+            Swal.fire(
+                'Deleted!',
+                'Your contract has been deleted.',
+                'success'
+            );
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire(
+                'Cancelled',
+                'Your contract is safe :)',
+                'error'
+            );
         }
-        localStorage.setItem('allContractsData', JSON.stringify(allContractsData));
-        renderContractList();
-    }
+    });
 }
 
 // Function to sort an array of objects by key
-function sortByKey(key, sortStatus){
+function sortByKey(key, sortStatus) {
     const originalArray = readContractsData();
 
-    if(key==='contractValue' && sortStatus==true){
-        const sortedArray = originalArray.sort(function(a,b){
-            let x=a[key];
-            let y=b[key];
-            let check = x-y;
-            return((check<0) ? -1 : ((check>1) ? 1 : 0));
+    if (key === 'contractValue' && sortStatus == true) {
+        const sortedArray = originalArray.sort(function (a, b) {
+            let x = a[key];
+            let y = b[key];
+            let check = x - y;
+            return ((check < 0) ? -1 : ((check > 1) ? 1 : 0));
         })
         return sortedArray;
-    } else if(key==='contractValue' && !(sortStatus==true)){
-        const sortedArray = originalArray.sort(function(a,b){
-            let x=a[key];
-            let y=b[key];
-            let check = x-y;
-            return((check>0) ? -1 : ((check<1) ? 1 : 0));
+    } else if (key === 'contractValue' && !(sortStatus == true)) {
+        const sortedArray = originalArray.sort(function (a, b) {
+            let x = a[key];
+            let y = b[key];
+            let check = x - y;
+            return ((check > 0) ? -1 : ((check < 1) ? 1 : 0));
         })
         return sortedArray;
-    } else if(!(key==='contractValue') && sortStatus==true){
-        const sortedArray = originalArray.sort(function(a,b){
-            let x=a[key].toUpperCase();
-            let y=b[key].toUpperCase();
-            return ((x<y) ? -1 : ((x>y) ? 1 : 0));
+    } else if (!(key === 'contractValue') && sortStatus == true) {
+        const sortedArray = originalArray.sort(function (a, b) {
+            let x = a[key].toUpperCase();
+            let y = b[key].toUpperCase();
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         })
         return sortedArray;
-    } else{
-        const sortedArray = originalArray.sort(function(a,b){
-            let x=a[key].toUpperCase();
-            let y=b[key].toUpperCase();
-            return ((x<y) ? 1 : ((x>y) ? -1 : 0));
+    } else {
+        const sortedArray = originalArray.sort(function (a, b) {
+            let x = a[key].toUpperCase();
+            let y = b[key].toUpperCase();
+            return ((x < y) ? 1 : ((x > y) ? -1 : 0));
         })
         return sortedArray;
     }
